539e35703cc83f257ee023fb32f34e34cebf9133
Fatih Yılmaz <fatih.yilmaz@mobilistbilisim.com>
2022-04-11 13:16:29 +0300

    finish

--- data/src/main/java/com/fyilmaz/data/Resource.kt
+++ data/src/main/java/com/fyilmaz/data/Resource.kt
@@ -1,5 +1,5 @@
 package com.fyilmaz.data
-sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+sealed class Resource<T>(val data: T? = null, val message: String? = null, val error: Exception? = null) {
     class Success<T>(data: T) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Error<T>(message: String, data: T? = null, error: Exception) : Resource<T>(data, message, error)
 }
